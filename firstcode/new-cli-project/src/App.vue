5b3e5e797e8e0a4cf7abcfdbef631f9587ed7564
newton.m <newton.m@marketdesigners.com>
2019-07-22 10:43:19 +0900

    Button-proto-first
    disabled button was not maked

---
+++ firstcode/new-cli-project/src/App.vue
@@ -0,0 +1,117 @@
+<template>
+  <center>
+  <div>
+    <br><br> <hr><hr><br>
+    <h1>마음에 드는 버튼 스타일을 골라보세요.</h1>
+    <br><hr><hr><br><br><br>
+    <hr>
+    <h2>이곳은 사각형의 버튼들을 모아놓은 공간입니다.</h2>
+    <hr><br>
+
+    <h3>1. 사각형의 큰버튼들입니다.</h3>
+    <br><br><br>
+    <AppButton :rounded="false" color="default" size="large">Default</AppButton> &nbsp;
+    <AppButton :rounded="false" color="blue" size="large">Primary</AppButton> &nbsp;
+    <AppButton :rounded="false" color="green" size="large">Success</AppButton> &nbsp;
+    <AppButton :rounded="false" color="gray" size="large">Info</AppButton> &nbsp;
+    <AppButton :rounded="false" color="yellow" size="large">Warning</AppButton> &nbsp;
+    <AppButton :rounded="false" color="red" size="large">Danger</AppButton>
+    <br><br><br><br>
+    <h3>사각형의 중간버튼들입니다.</h3>
+    <br><br><br>
+    <AppButton :rounded="false" color="default" size="medium">Default</AppButton> &nbsp;
+    <AppButton :rounded="false" color="blue" size="medium">Primary</AppButton> &nbsp;
+    <AppButton :rounded="false" color="green" size="medium">Success</AppButton> &nbsp;
+    <AppButton :rounded="false" color="gray" size="medium">Info</AppButton> &nbsp;
+    <AppButton :rounded="false" color="yellow" size="medium">Warning</AppButton> &nbsp;
+    <AppButton :rounded="false" color="red" size="medium">Danger</AppButton>
+    <br><br><br><br>
+    <h3>사각형의 작은버튼들입니다.</h3>
+    <br><br><br>
+    <AppButton :rounded="false" color="default" size="small">Default</AppButton> &nbsp;
+    <AppButton :rounded="false" color="blue" size="small">Primary</AppButton> &nbsp;
+    <AppButton :rounded="false" color="green" size="small">Success</AppButton> &nbsp;
+    <AppButton :rounded="false" color="gray" size="small">Info</AppButton> &nbsp;
+    <AppButton :rounded="false" color="yellow" size="small">Warning</AppButton> &nbsp;
+    <AppButton :rounded="false" color="red" size="small">Danger</AppButton>
+    <br><br><br><br>
+    <hr>
+    <h2>이곳은 둥근모양의 버튼들을 모아놓은 공간입니다.</h2>
+    <hr><br>
+     <h3>둥근모양의 큰버튼들입니다.</h3>
+    <br><br><br>
+    <AppButton :rounded="true" color="default" size="large">Default</AppButton> &nbsp;
+    <AppButton :rounded="true" color="blue" size="large">Primary</AppButton> &nbsp;
+    <AppButton :rounded="true" color="green" size="large">Success</AppButton> &nbsp;
+    <AppButton :rounded="true" color="gray" size="large">Info</AppButton> &nbsp;
+    <AppButton :rounded="true" color="yellow" size="large">Warning</AppButton> &nbsp;
+    <AppButton :rounded="true" color="red" size="large">Danger</AppButton>
+    <br><br><br><br>
+    <h3>둥근모양의 중간버튼들입니다.</h3>
+    <br><br><br>
+    <AppButton :rounded="true" color="default" size="medium">Default</AppButton> &nbsp;
+    <AppButton :rounded="true" color="blue" size="medium">Primary</AppButton> &nbsp;
+    <AppButton :rounded="true" color="green" size="medium">Success</AppButton> &nbsp;
+    <AppButton :rounded="true" color="gray" size="medium">Info</AppButton> &nbsp;
+    <AppButton :rounded="true" color="yellow" size="medium">Warning</AppButton> &nbsp;
+    <AppButton :rounded="true" color="red" size="medium">Danger</AppButton>
+    <br><br><br><br>
+    <h3>둥근모양의 작은버튼들입니다.</h3>
+    <br><br><br>
+    <AppButton :rounded="true" color="default" size="small">Default</AppButton> &nbsp;
+    <AppButton :rounded="true" color="blue" size="small">Primary</AppButton> &nbsp;
+    <AppButton :rounded="true" color="green" size="small">Success</AppButton> &nbsp;
+    <AppButton :rounded="true" color="gray" size="small">Info</AppButton> &nbsp;
+    <AppButton :rounded="true" color="yellow" size="small">Warning</AppButton> &nbsp;
+    <AppButton :rounded="true" color="red" size="small">Danger</AppButton>
+    <br><br><br><br>
+    <hr>
+    <h2>이곳은 플레인 형태의 버튼들을 모아놓은 공간입니다.</h2>
+    <hr><br>
+    <h3>플레인 형태의 큰버튼들입니다.</h3>
+    <br><br><br>
+    <AppButton :rounded="false" color="default" size="large">Default</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainBlue" size="large">Primary</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainGreen" size="large">Success</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainGray" size="large">Info</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainYellow" size="large">Warning</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainRed" size="large">Danger</AppButton>
+    <br><br><br><br>
+    <h3>플레인 형태의 중간버튼들입니다.</h3>
+    <br><br><br>
+    <AppButton :rounded="false" color="default" size="medium">Default</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainBlue" size="medium">Primary</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainGreen" size="medium">Success</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainGray" size="medium">Info</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainYellow" size="medium">Warning</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainRed" size="medium">Danger</AppButton>
+    <br><br><br><br>
+    <h3>플레인 형태의 작은버튼들입니다.</h3>
+    <br><br><br>
+    <AppButton :rounded="false" color="default" size="small">Default</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainBlue" size="small">Primary</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainGreen" size="small">Success</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainGray" size="small">Info</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainYellow" size="small">Warning</AppButton> &nbsp;
+    <AppButton :rounded="false" color="plainRed" size="small">Danger</AppButton>
+    <br><br><br><br> 
+  </div>
+  </center>
+</template>
+
+<script>
+
+import AppButton from './components/Button.vue'
+
+export default {
+  components: {
+    AppButton
+  },
+  name: 'App',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ firstcode/new-cli-project/src/components/Button.vue
@@ -0,0 +1,142 @@
+<template>
+  <Appbutton 
+    :class="[
+        'button',
+        'button--' + color,
+        'button--' + size,
+        'button--' + type,
+        {
+            'button--rounded':rounded
+        },
+        {
+            'button--disabled':disabled
+        }
+        ]"
+    >
+    <slot></slot>
+  </Appbutton>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: {
+      color: {
+          type : String,
+          default: "default",
+          validator(x) {
+              return ["blue", "green", "default", "gray", "yellow", "red"].indexOf(x) !==-1;
+          }
+      },
+      rounded: {
+          type: Boolean,
+          default: false
+          
+      },
+      disabled: {
+          type: Boolean,
+          default: false
+
+      },
+      
+      size: {
+          type: String,
+          default: "default",
+          validator(x) {
+              return ["small", "medium", "default"].indexOf(x) !==-1;
+          }
+      }
+
+  }
+  
+}
+</script>
+
+<style scoped>
+.button {
+    display: in-line-block;
+    outline: 0;
+    border: 1px solid rgba(0, 0, 0, 0.5);
+    color: #000000;
+    font-weight: 500;
+    text-align: center;
+    vertical-align: middle;
+    border-radius: 10px;
+    
+}
+
+.plainButton
+
+.button--blue {
+    background-color: #409eff
+}
+
+.button--default {
+    background-color: #ffffff
+}
+.button--green {
+    background-color: #67c23a
+}
+.button--gray {
+    background-color: #909399
+}
+.button--yellow {
+    background-color: #e6a23c
+}
+.button--red {
+    background-color: #f56c6c
+}
+
+.button--blue {
+    background-color:#409eff
+}
+
+.button--plainRed {
+    background-color: #fedfdf;
+    border: 1px solid #ee6363;
+    color: #ee6363;
+}
+.button--plainBlue{
+    background-color: #dfdffe;
+    border: 1px solid #409eff;
+    color: #409eff;
+}
+.button--plainGreen{
+    background-color: #dffedf;
+    border: 1px solid #67c23a;
+    color: #67c23a;
+}
+.button--plainGray{
+    background-color: #cacaca;
+    border: 1px solid #6d6b6b;
+    color: #6d6b6b;
+}
+.button--plainYellow{
+    background-color: #fffec5;
+    border: 1px solid #e6a23c;
+    color: #e6a23c;
+}
+
+.button--small {
+    padding: 10px 20px;
+    font-size : 12px;
+}
+
+.button--medium {
+    padding: 15px 25px;
+    font-size : 14px;
+}
+
+.button--large {
+    padding: 20px 30px;
+    font-size : 14px;
+}
+
+.button--rounded {
+    border-radius: 60px;
+}
+
+.button--disabled {
+}
+
+</style>
